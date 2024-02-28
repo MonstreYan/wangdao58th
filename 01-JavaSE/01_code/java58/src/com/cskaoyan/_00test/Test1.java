4cd729432d38b4c4887b8ccdff70798e6f4c6bfd
Common-zhou <1050015785@qq.com>
2024-02-28 21:53:11 +0800

    code

---
+++ 01-JavaSE/01_code/java58/src/com/cskaoyan/_00test/Test1.java
@@ -0,0 +1,11 @@
+package com.cskaoyan._00test;
+
+/**
+ * @author Common-zhou
+ * @since 2024-02-28 21:52
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("hello world ad");
+    }
+}
