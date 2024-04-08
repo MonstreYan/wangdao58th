2fe40e73712061d7f7b7bf82ce871f41137894c0
Common-zhou <1050015785@qq.com>
2024-04-08 17:32:21 +0800

    这是测试提交

---
+++ 01-JavaSE/01_code/java58/src/com/cskaoyan/_21stream/_04endstream/Test1.java
@@ -0,0 +1,11 @@
+package com.cskaoyan._21stream._04endstream;
+
+/**
+ * @author Common-zhou
+ * @since 2024-04-08 17:30
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("hhhhhh");
+    }
+}
