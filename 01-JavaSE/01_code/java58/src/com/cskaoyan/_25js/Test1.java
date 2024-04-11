bf42e026f193143a880dd27d02fe2a9ed8a37ef1
Common-zhou <1050015785@qq.com>
2024-04-11 12:05:41 +0800

    code

---
+++ 01-JavaSE/01_code/java58/src/com/cskaoyan/_25js/Test1.java
@@ -0,0 +1,36 @@
+package com.cskaoyan._25js;
+
+/**
+ * 取名字的时候，要注意一下。  Test   System  String。
+ *
+ * @author Common-zhou
+ * @since 2024-04-11 11:22
+ */
+public class Test1 {
+    Test1 test1;
+
+    // @org.junit.Test
+    // public void test1() {
+    //     System.out.println();
+    // }
+
+    public static void main(String[] args) {
+        // 在Java中，哪些东西能放到if里面？
+
+        // if ("hello") {
+        // }
+        // if (10) {
+        // }
+
+        // 要么是一个布尔值，要么布尔值表达式。 在Java中。
+    }
+
+
+    public void test1() {
+        int[] nums = {1, 2, 3};
+
+        for (int num : nums) {
+
+        }
+    }
+}
